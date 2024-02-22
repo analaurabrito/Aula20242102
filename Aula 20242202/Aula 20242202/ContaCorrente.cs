a23d350065b25fffb3200ab1c186223312747286
Aura <111578898+analaurabrito@users.noreply.github.com>
2024-02-22 08:35:24 -0300

    Add files via upload

---
+++ Aula 20242202/Aula 20242202/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20242202
+{
+    internal class ContaCorrente
+    {
+        private Proprietario proprietario;
+        public double saldo { get; private set; }
+        private string log { get; set; }
+
+        public ContaCorrente(Proprietario proprietario, double saldo)
+            {
+            this.proprietario = proprietario;
+            this.saldo = saldo;
+            this.log = "EXTRATO DE "+this.proprietario+ "\n";
+        }
+
+        public Boolean Sacar(double valor_saque)
+        {
+            if (saldo >= valor_saque)
+            {
+                saldo =- valor_saque;
+                Console.WriteLine("Saque realizado.\n");
+                this.log += "\nSaque realizado no valor " +valor_saque.ToString();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente.\n");
+                this.log += "\nTentativa de Saque mal sucedida".ToString();
+                return false;
+            }
+        }
+
+        public Boolean Depositar(double valor_deposito)
+        {
+            saldo =+ valor_saque;
+            Console.WriteLine("Deposito realizado.");
+            this.log += "\Deposito realizado no valor " + valor_saque.ToString();
+            return true;
+        }
+
+    }
+}
---
+++ Aula 20242202/Aula 20242202/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20242202
+{
+    internal class Endereco
+    {
+        private string rua { get; set; }
+        private int numero { get; set; }
+
+        public Endereco(string rua, int numero)
+        {
+            this.rua = rua;
+            this.numero = numero;
+        }
+    }
+}
